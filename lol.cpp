95ee304c9f3d416b6f979abaea0f3f758afd155f
Captain Gary Pecker <nachalnike.da@gmail.com>
2018-09-12 10:01:26 +0300

    ветка разработки генератора случайностей

--- lol.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  lol.cpp
-//  projectBigMac
-//jwrjwenknweflwemkwef
-//  Created by Captain Gary Pecker on 11.09.2018.
-//  Copyright © 2018 Captain Gary Pecker. All rights reserved.
-//
-
-#include <stdio.h>
---
+++ testRandom/testRandom/testRandom.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testRandom
+//
+//  Created by Captain Gary Pecker on 12.09.2018.
+//  Copyright © 2018 Captain Gary Pecker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
